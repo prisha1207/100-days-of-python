3e55e439fb9490ee3a374450864b028f1a3fa339
prisha banerjee <bprisha123@gmail.com>
2026-07-28 22:05:20 +0530

    Completed Day 9 exercises

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,30 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+import os
 
+def highest_bidder(record):
+    higest_bid=0
+    winner=""
+    for bid in record:
+        amount=record[bid]
+        if amount>higest_bid:
+            higest_bid=amount
+            winner=bid
+
+    print(f"The winner is {winner} with the highest bid of ${higest_bid}")
+
+
+auction={}
+
+auction_count=True
+while auction_count:
+    name = input("What's your name: ")
+    price = float(input("Enter your big price: $"))
+    auction[name] = price
+    choice = input(print("Do you want to place a new bid? \nY for yes N for no --> "))
+    if choice == 'N':
+        auction_count = False
+        highest_bidder(auction)
+    else:
+        print("\n"*30)
