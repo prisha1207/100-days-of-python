7ae6daadbe67a41604e67439e109d0230df861ff
prisha banerjee <bprisha123@gmail.com>
2026-07-28 23:53:43 +0530

    Completed Day 10 exercise

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,40 @@
 def add(n1, n2):
     return n1 + n2
+def sub(n1,n2):
+    return n1-n2
+def multi(n1,n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
+
+operations={'+': add, '-':sub, '*':multi, '/':div }
+
+import art
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
+
 
 
